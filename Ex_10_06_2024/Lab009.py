ed5741d40f334254f756b65e398c3efd1a397f02
Malharsahoo <smalhar96@gmail.com>
2024-06-14 17:56:11 +0530

    set data type and area of circle using math library

---
+++ Ex_10_06_2024/Lab009.py
@@ -0,0 +1,6 @@
+# Set data type
+
+my_set={1,2,3,4,3,5,5}
+print(my_set)
+print(type(my_set))
+#Re= {1.2,3,4,5}   & <class  'set'>
---
+++ Ex_10_06_2024/Lab010.py
@@ -0,0 +1,19 @@
+# area of circle using math library & single line operation logic
+
+
+import math
+'''radius= float(input("enter the value of radius\n"))
+print(math.pi)
+area1=math.pi*(radius**2)
+print("the area of circle is",area1)
+print(type(area1))
+area2=math.pi*(math.pow(radius,2))
+print("circle area is",area2)
+print(type(area2))'''
+# result= enter the value of radius 5, 3.141592653589793,the area of circle is 78.53981633974483
+#<class 'float'>,circle area is 78.53981633974483,<class 'float'>
+
+
+#single line operation
+print((math.pi)*(float(input("enter the value of radius\n"))**2))
+#enter the value of radius 3, 28.274333882308138
