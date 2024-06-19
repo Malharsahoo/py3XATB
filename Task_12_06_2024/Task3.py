2e32e6963c4c5dd96bd8072327b4a2848d2e671d
Malharsahoo <smalhar96@gmail.com>
2024-06-19 16:49:30 +0530

    factorial using for loop and while loop

--- Task_12_06_2024/Task3.py
+++ Task_12_06_2024/Task3.py
@@ -1,11 +1,21 @@
-# Factorial of a number using for loop
+# Factorial of a number using for loop and while
 
 
-number = int(input("enter a number: \n"))
-result=1
-for number in range(1, number + 1):
- result = number * result
-print(f"The Factorial of {number} is:{result}")
+#number = int(input("enter a number: \n"))
+#result=1
+#for number in range(1, number + 1):
+# result = number * result
+#print(f"The Factorial of {number} is:{result}")
+
+
+
+num=int(input("enter a number\n"))
+fact=1
+while(num>0):
+ fact=fact*num
+ num=num-1
+print(f"factorial of number is: {fact}")
+
 
 
 
