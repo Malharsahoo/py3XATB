cb1ab153da6f8d18ed568f89ef64d3bb281993c8
meghamalhar1 <mmalhar247@gmail.com>
2025-10-07 21:37:14 +0530

    lab 003

--- Ex_05_06_2024/Lab003.py
+++ Ex_05_06_2024/Lab003.py
@@ -1,3 +1,4 @@
 # separator and end with examples
-print("I","am","a","boy",sep="-", end="\t")
-print("my","name","is")
+print("I", "am", "a", "boy", sep="-", end="\t")
+print("my", "name", "is")
+print("hello", "malhar", "sahoo", end="\n" "ha ha", sep="_")
---
+++ Ex_05_06_2024/Lab003_1EX.py
@@ -0,0 +1,5 @@
+# take input from the user and print it
+
+first_name=input("enter your first name")
+last_name=input("enter your last name")
+print("your first name is",first_name,"your last name is",last_name, sep="\n")
